77c573b28712b76d9dea035ec5e9bfcb1e5f107b
Jennifer Lee <jenlee.tech@gmail.com>
2024-05-22 18:14:44 -0400

    correct spelling

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     "restaurant/static",
 ]
 
-STATIC_ROOT = os.path.joing(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
