606b53bf8c6f143049f96522cd6d2522fc552ef5
Jennifer Lee <jenlee.tech@gmail.com>
2024-05-22 14:56:53 -0400

    change debug feature

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r-%0eqcja86asp_+*1@3d6t9wm65$7^p*v^m961p@)w7*3ob0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # The settings for static files have been updated for the Graded assessment
-STATIC_URL = ['restaurant/static/', BASE_DIR / 'staticfiles']
+STATIC_URL = 'restaurant/static/'
 
 STATICFILES_DIRS = [
     "restaurant/static",
