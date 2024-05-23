37dfc98e535a9db7daee342c596772d284fac4b3
Jennifer Lee <jenlee.tech@gmail.com>
2024-05-22 23:46:45 -0400

    changed build files

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
+echo "BUILD START"
+python3.9 -m venv venv
+source venv\Scripts\activate
 pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic -- noinput
+echo "BUILD END"
