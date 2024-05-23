f4f1ca002b59110c27070fc8bbca3313740b6eaa
Jennifer Lee <jenlee.tech@gmail.com>
2024-05-23 00:07:08 -0400

    removed build files

--- build_files.sh
+++
@@ -1,5 +0,0 @@
-echo "BUILD START"
-python3.9 -m venv venv
-pip3 install -r requirements.txt 
-python3.9 manage.py collectstatic -- noinput
-echo "BUILD END"
--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r-%0eqcja86asp_+*1@3d6t9wm65$7^p*v^m961p@)w7*3ob0n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
